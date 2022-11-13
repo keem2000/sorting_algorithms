1e5e58b308ae822811a421063ad97eaa6c34b652
keem2000 <joycelinaomari@gmail.com>
2022-11-12 18:33:23 -0800

    algorithms

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -2,52 +2,78 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * _merge_sort - initiate merge sort
- * @array: array to be sorted
- * @temp: temporary array for holding sorted elements
- * @size: size of the array
+ * merge_sort - A function that sorts an array using merge algorithm.
+ * @array: The array to sort.
+ * @size: The size of the array.
+ * Return: Nothing.
  */
-void _merge_sort(int *array, int *temp, size_t size)
+void merge_sort(int *array, size_t size)
 {
-size_t half = size / 2, i = 0, j = 0, k;
-if (size < 2)
+size_t i = 0;
+int *base = NULL;
+if (array == NULL || size < 2)
 return;
-_merge_sort(array, temp, half);
-_merge_sort(array + half, temp + half, size - half);
+base = malloc(sizeof(int) * size);
+if (base == NULL)
+return;
+for (; i < size; i++)
+base[i] = array[i];
+merge_partition(0, size, array, base);
+free(base);
+}
+/**
+ * merge - A function that sorts the subarrays.
+ * @lo: Lower index.
+ * @mi: Middle index.
+ * @hi: Higher index.
+ * @dest: Destination for data.
+ * @src: Input data.
+ * Return: Nothing
+ */
+void merge(size_t lo, size_t mi, size_t hi, int *dest, int *src)
+{
+size_t i = 0, j = 0, k = 0;
 printf("Merging...\n");
 printf("[left]: ");
-print_array(array, half);
+print_array(src + lo, mi - lo);
 printf("[right]: ");
-print_array(array + half, size - half);
-for (k = 0; k < size; k++)
-if (j >= size - half || (i < half && array[i] < (array + half)[j]))
+print_array(src + mi, hi - mi);
+i = lo;
+j = mi;
+k = lo;
+for (; k < hi; k++)
 {
-temp[k] = array[i];
+if (i < mi && (j >= hi || src[i] <= src[j]))
+{
+dest[k] = src[i];
 i++;
 }
 else
 {
-temp[k] = (array + half)[j];
+dest[k] = src[j];
 j++;
 }
-for (k = 0; k < size; k++)
-array[k] = temp[k];
+}
 printf("[Done]: ");
-print_array(array, size);
+print_array(dest + lo, hi - lo);
 }
 /**
- * merge_sort - initiate merge sort
- * @array: array to be sorted
- * @size: size of the array
+ * merge_partition - A funtion that splits the array recursively.
+ * @lo: Lower index
+ * @hi: Higher index.
+ * @array: The array to sort.
+ * @base: The copy of the array.
+ * Return: Nothing.
  */
-void merge_sort(int *array, size_t size)
+void merge_partition(size_t lo, size_t hi, int *array, int *base)
 {
-int *temp;
-if (!array || size < 2)
+size_t mi = 0;
+if (hi - lo < 2)
 return;
-temp = malloc(sizeof(*temp) * size);
-if (!temp)
-return;
-_merge_sort(array, temp, size);
-free(temp);
+mi = (lo + hi) / 2;
+merge_partition(lo, mi, array, base);
+merge_partition(mi, hi, array, base);
+merge(lo, mi, hi, array, base);
+for (mi = lo; mi < hi; mi++)
+base[mi] = array[mi];
 }
